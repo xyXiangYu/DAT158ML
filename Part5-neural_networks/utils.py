c3b69893d89fd14fd7da009a4c0ec037fba41e2a
Alexander Lundervold <alexander.lundervold@gmail.com>
2018-11-15 13:38:57 +0100

    nn update

--- Part5-neural_networks/utils.py
+++ Part5-neural_networks/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
 import torch
 import torchvision
 from torchvision import transforms
@@ -33,3 +34,31 @@ def plot_cifar10():
         plt.imshow(img)
         plt.show()
         print(f'{[classes[labels[j]] for j in range(4)]}')
+        
+        
+        
+def plot_confusion_matrix(cm, classes, ax=None,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues, labels=True):
+    """
+    This function prints and plots the confusion matrix.
+    """
+    
+    if not ax: fig, ax = plt.subplots(figsize=(8,6))
+    ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    tick_marks = np.arange(len(classes))
+    plt.setp(ax, xticks=tick_marks, xticklabels=classes, 
+             yticks=tick_marks, yticklabels=classes,
+             title=title, xlabel="Predicted label", 
+             ylabel="True label")
+
+    if labels:
+        fmt = 'd'
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            ax.text(j, i, format(int(cm[i, j]), fmt),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    return ax
